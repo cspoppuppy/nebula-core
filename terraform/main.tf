bc35aa9d0acbb1f8ce5d72e637e3e6f80700c0fb
cspoppuppy <cspoppuppy@hotmail.com>
2026-01-26 22:24:37 +0000

    terraform - cloudflare

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,46 +1,36 @@
-terraform {
-  required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "cloudflare" {
-  api_token = var.cloudflare_api_token
-}
-
 # Create a Cloudflare Tunnel for the cluster
-resource "cloudflare_tunnel" "k8s_tunnel" {
-  account_id = var.cloudflare_account_id
-  name       = "nebula-cluster-tunnel"
-  secret     = file(var.tunnel_secret_path) # Recommended to generate this securely
+resource "cloudflare_zero_trust_tunnel_cloudflared" "k8s_tunnel" {
+  account_id    = var.cloudflare_account_id
+  name          = "nebula-cluster-tunnel"
+  tunnel_secret = file(var.tunnel_secret_path) # Recommended to generate this securely
 }
 
 # Documentation: This assumes you have `cloudflared` running in K8s (GitOps)
 # We just create the CNAMEs here to point to the tunnel.
 
-resource "cloudflare_record" "argocd" {
+resource "cloudflare_dns_record" "argocd" {
   zone_id = var.cloudflare_zone_id
   name    = "argocd"
-  value   = "${cloudflare_tunnel.k8s_tunnel.id}.cfargotunnel.com"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.k8s_tunnel.id}.cfargotunnel.com"
   type    = "CNAME"
   proxied = true
+  ttl     = 1
 }
 
-resource "cloudflare_record" "grafana" {
+resource "cloudflare_dns_record" "grafana" {
   zone_id = var.cloudflare_zone_id
   name    = "grafana"
-  value   = "${cloudflare_tunnel.k8s_tunnel.id}.cfargotunnel.com"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.k8s_tunnel.id}.cfargotunnel.com"
   type    = "CNAME"
   proxied = true
+  ttl     = 1
 }
 
-resource "cloudflare_record" "jupyter" {
+resource "cloudflare_dns_record" "jupyter-hub" {
   zone_id = var.cloudflare_zone_id
-  name    = "hub"
-  value   = "${cloudflare_tunnel.k8s_tunnel.id}.cfargotunnel.com"
+  name    = "jupyter-hub"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.k8s_tunnel.id}.cfargotunnel.com"
   type    = "CNAME"
   proxied = true
+  ttl     = 1
 }
